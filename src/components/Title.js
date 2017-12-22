3f9f88747eee48abcb7a8ec54dc5de0d2fa56975
Basiakus <panadambasiak@gmail.com>
2017-12-22 11:52:12 +0100

    commit

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,19 @@
+import React from 'react';
+
+class Title extends React.Component {
+	
+	render() {
+	return (
+		<h1>{this.props.text}</h1>
+		);
+	}
+}
 
 
+
+Title.propsTypes = {
+ 	title: React.PropTypes.string.isRequired
+}
 
 
 export default Title;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import style from './App.css';
+import Title from '../components/Title.js';
 
 
 class App extends React.Component {
@@ -24,7 +25,7 @@ class App extends React.Component {
     render() {
         return (
             <div className={style.TodoApp}>
-                
+                <Title title='Tytuł aplikacji' />
                 Tutaj pojawią się komponenty naszej aplikacji.
             </div>
         );
