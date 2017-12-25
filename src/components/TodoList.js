2ed5834324b59ebe042dcbe2fe2362edf5fdbcc5
Basiakus <panadambasiak@gmail.com>
2017-12-25 11:34:38 +0100

    removeTodo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import style from './TodoList.css';
-const TodoList = props => <ul className={style.List}>{props.list.map(oneThing => <li key={oneThing.id} onClick={props.action}>{oneThing.text}</li>)} </ul>
+const TodoList = props => <ul className={style.List}>{props.list.map(oneThing => <li key={oneThing.id} onClick={props.action(oneThing.id)}>{oneThing.text}</li>)} </ul>
 TodoList.propsTypes = {
  	list: PropTypes.object.isRequired,
  	action: PropTypes.func.isRequired
